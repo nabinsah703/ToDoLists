a21079442cafa1b04a3147c08b550a330f352314
nabin sah <nabinsah703@gmail.com>
2023-11-29 08:32:53 +0545

    add seed method and enable migration and add migration and update database

--- ToDoList/Models/ToDo.cs
+++ ToDoList/Models/ToDo.cs
@@ -10,7 +10,7 @@ namespace ToDoList.Models
     {
         public int Id { get; set; }
         public string Description { get; set; }
-        public bool isDone { get; set; }
-        public virtual ApplicationUser user { get; set; }
+        public bool IsDone { get; set; }
+        public virtual ApplicationUser User { get; set; }
     }
 }
